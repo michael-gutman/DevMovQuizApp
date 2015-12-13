17824cda586dc62c3e47c921129bcbf498742c71
michael-gutman <michaelgutman@Hotmail.com>
2015-12-12 19:22:06 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# DevMovQuizApp
+5 T/F question quiz app in C# for #DevMov Student "Challenge #4 Quiz application" (November 18 – December 13)
---
+++ devMovQuizApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace devMovQuizApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = 0;
+            string attempt;
+            string[] questions = 
+                {
+                "Q1) Carl Lewis holds the record for most individual gold medals at the Olympics.",
+                "Q2) The Academy Awards are annually the most-watched, single telecast in the United States.",
+                "Q3) Russia has the largest area of any country in the world.",
+                "Q4) The 'funny bone' is really a bone.",
+                "Q5) The Enlightenment was an intellectual movement that celebrated religious faith over reason."
+                };
+            string[] answers = { "false", "false", "true", "false", "false" };
+            foreach (string i in questions)
+            {
+                Console.WriteLine(i);
+                Console.ReadLine();
+            };
+        }
+    }
+}
